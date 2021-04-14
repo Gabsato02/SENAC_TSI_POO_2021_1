f7ab45b8cc8c2bd116b1fed6e2ce778632a1f252
Gabriel Sato <gabsato02@gmail.com>
2021-04-13 22:20:33 -0300

    Aula de 13/04

--- aula-06-04/estoque.php
+++ aula-06-04/estoque.php
@@ -10,10 +10,48 @@ class Main {
         echo "\n\n --- COMEÇO DO PROGRAMA --- \n\n";
 
         $objUsuario = new Usuario;
+
         $objFabricante = new Fabricante;
         $objEstoque = new Estoque;
         $objMovimentacao = new Movimentacao;
 
+        switch($_SERVER['argv']['1']) {
+            case 'gravarUsuario':
+                $this->gravarUsuario($objUsuario);
+                break;
+            case 'editarUsuario':
+                $this->editarUsuario($objUsuario);
+                break;
+            default:
+            echo "\nErro: Não existe a funcionalidade {$_SERVER['argv'][1]}\n";
+        }
+    }
+
+    public function gravarUsuario($objUsuario) {
+        $dados = $this->tratarDados();
+        $objUsuario->setDados($dados);
+        if ($objUsuario->gravarDados()) {
+            echo "\nUsuário gravado com sucesso!\n";
+        }
+    }
+
+    public function editarUsuario($objUsuario) {
+        $dados = $this->tratarDados();
+        $objUsuario->setDados($dados);
+        if ($objUsuario->gravarDados()) {
+            echo "\nUsuário editado com sucesso!\n";
+        }
+    }
+
+    public function tratarDados():array {
+        $args = explode(',', $_SERVER['argv'][2]);
+                        
+        foreach ($args as $valor) {
+            $arg = explode('=', $valor);
+            $dados[$arg[0]] = $arg[1];
+        }
+
+        return $dados;
     }
 
     public function __destruct() {
